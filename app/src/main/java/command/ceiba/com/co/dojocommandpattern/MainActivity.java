7f41bc134b7cd4214e2667ceea174211a22db269
Esteban Higuita <esteban.higuita@ceiba.com.co>
2019-06-05 23:20:29 -0500

    Procesar notificaciones a diferentes actividades

--- app/src/main/java/command/ceiba/com/co/dojocommandpattern/MainActivity.java
+++ app/src/main/java/command/ceiba/com/co/dojocommandpattern/MainActivity.java
@@ -1,5 +1,8 @@
 package command.ceiba.com.co.dojocommandpattern;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            String channelId  = getString(R.string.default_notification_channel_id);
+            String channelName = getString(R.string.default_notification_channel_name);
+            NotificationManager notificationManager =
+                    getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(new NotificationChannel(channelId,
+                    channelName, NotificationManager.IMPORTANCE_LOW));
+        }
+
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
 
@@ -32,13 +44,10 @@ public class MainActivity extends AppCompatActivity {
                             return;
                         }
 
-                        // Get new Instance ID token
                         String token = Objects.requireNonNull(task.getResult()).getToken();
 
-                        // Log and toast
                         String msg = "token de firebase " + token;
                         Log.d(TAG, msg);
-                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
                     }
                 });
     }
--- app/src/main/java/command/ceiba/com/co/dojocommandpattern/MessagingService.java
+++ app/src/main/java/command/ceiba/com/co/dojocommandpattern/MessagingService.java
@@ -1,23 +1,58 @@
 package command.ceiba.com.co.dojocommandpattern;
 
-import android.util.Log;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Map;
+
 public class MessagingService extends FirebaseMessagingService {
 
-    private static String TAG = "MessagingService";
-    public MessagingService(){
+    private static final String firstActivity = "first.activity";
+    private static final String secondActivity = "second.activity";
 
-    }
+    private String key;
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-        Log.i(TAG, "mensaje de notificación " + remoteMessage);
-        Log.i(TAG, "mensaje de notificación " + remoteMessage.getData());
-        Log.i(TAG, "mensaje de notificación body " + remoteMessage.getNotification().getBody());
-        Log.i(TAG, "mensaje de notificación key " + remoteMessage.getData().get("key"));
+        Map<String, String> notification = remoteMessage.getData();
+
+        String title = notification.get("title");
+        String body = notification.get("body");
+
+        key = notification.get("key");
+        if(key != null) {
+            switch (key){
+                case firstActivity:
+                    Intent firstActivityIntent = new Intent(this, FirstActivity.class);
+                    buildNotification(title, body, firstActivityIntent);
+                    break;
+                case secondActivity:
+                    Intent secondActivityIntent = new Intent(this, SecondActivity.class);
+                    buildNotification(title, body, secondActivityIntent);
+            }
+        }
+
+
+    }
+
+    private void buildNotification(String title, String body, Intent intent) {
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, intent, 0);
+        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+
+        Notification notification = new NotificationCompat.Builder(this, getString(R.string.default_notification_channel_id))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setContentTitle(title)
+                .setContentText(body)
+                .setAutoCancel(true)
+                .build();
+        notificationManager.notify(1, notification);
     }
 }
