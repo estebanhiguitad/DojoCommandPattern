9ab3b3a02323b9c1e718eb85f6a9d7933408e9e7
Esteban Higuita <estebanhiguitad@gmail.com>
2021-09-02 14:17:00 -0500

    Agregar número a la segunda actividad

--- app/src/main/java/command/ceiba/com/co/dojocommandpattern/SecondActivity.kt
+++ app/src/main/java/command/ceiba/com/co/dojocommandpattern/SecondActivity.kt
@@ -2,16 +2,35 @@ package command.ceiba.com.co.dojocommandpattern
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 
 class SecondActivity : AppCompatActivity() {
+
+    private lateinit var floatingActionButton: FloatingActionButton
+    private lateinit var textViewRecordId: TextView
+    private var recordId : String? = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
-        val fab: FloatingActionButton = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
+
+        getExtras()
+        setupViews()
+    }
+
+    private fun getExtras() {
+        recordId = intent.getStringExtra("recordId")
+    }
+
+    private fun setupViews() {
+        floatingActionButton = findViewById(R.id.fab)
+        textViewRecordId = findViewById(R.id.record_id)
+
+        textViewRecordId.text = recordId
+        floatingActionButton.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
